34041e8aecb7663e7151072e11cd7fb8cc57bc76
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-16 14:19:49 +0930

    abstract class

---
+++ chip.h
@@ -0,0 +1,19 @@
+#ifndef CASINO_H
+#define CASINO_H 
+
+#include <iostream> 
+#include "casino.h"
+uusing namespace std; 
+
+class chip : public casino{
+
+//this class will be an abstract class that contains no data members, only pure virtual functions that determine how many chips are won and how many chips are placed in a bet 
+
+public: 
+virtual void plus_chips() = 0;    //these functions will have differtent implementations in each game
+virtual void minus_chips() = 0;
+
+}:
+
+
+#endif
