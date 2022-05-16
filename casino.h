3c3e0d00e1084178018757f9804a4f39f456843e
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-16 14:52:49 +0930

    edit

--- casino.h
+++ casino.h
@@ -14,14 +14,10 @@ int game;
 
 
 public: 
-<<<<<<< HEAD
     casino();
     int get_chips(); 
     int get_game();
-=======
-    casino(int _chips);
->>>>>>> e63e35f99c63bb6c8a7ea4bfce1204b722fa385c
-
+    
 };
 
 
