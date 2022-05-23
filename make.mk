4fc4bf31b022b80169a4474f1ef87539c3d95833
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 11:22:51 +0930

    yrd

--- make.mk
+++ make.mk
@@ -1,4 +1,4 @@
 git add main.cpp casino.cpp casino.h blackjack.cpp blackjack.h chip.cpp chip.h play.cpp play.h deck.cpp deck.h
 git commit -m "made with makefile"
-g++ git add main.cpp casino.cpp casino.h blackjack.cpp blackjack.h chip.cpp chip.h play.cpp play.h deck.cpp deck.h -o 1
+g++ main.cpp casino.cpp casino.h blackjack.cpp blackjack.h chip.cpp chip.h play.cpp play.h deck.cpp deck.h -o 1
 ./1
