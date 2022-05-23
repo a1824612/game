34fbfe4d0652c7ca53d987e00cc60ceed0ba9da5
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 11:43:21 +0930

    made with makefile

--- chip.h
+++ chip.h
@@ -5,7 +5,7 @@
 #include "casino.h"
 using namespace std; 
 
-class chip : public casino{
+class chip : public casino {
 
 //this class will be an abstract class that contains no data members, only pure virtual functions that determine how many chips are won and how many chips are placed in a bet 
 
