5db4d064c2b9905af156c46872bcbe0a92545dd0
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 13:14:18 +0930

    made with makefile

--- play.cpp
+++ play.cpp
@@ -6,36 +6,43 @@ using namespace std;
 void Play::plus_chips(){cout<<"arsse";};    //these functions will have differtent implementations in each game
 void Play::minus_chips(){cout<<"arsse";};
 void Play::run(){
-
-     blackjack b;
-    int lost = 0;
-    while (lost == 0) { //somehow move all of this into its own function
-        string hitsit = "NULL";
-        cout<<"Sit or hit?"<<endl; 
-        cin>>hitsit;
-        if (hitsit == "?") {
-            b.print_hand();
-        }
-        else if(hitsit == "sit"){
-            if(b.compare()) {
-                cout << "You lose!" << endl;
-                //lose chips
-                lost = 1;
+    string yesno;
+    cout << "Start Game: ";
+    cin << 
+    while (yesno == "yes") {
+        blackjack b;
+        int gameStatus = 0;
+        while (gameStatus == 0) { //somehow move all of this into its own function
+            string hitsit = "NULL";
+            cout<<"Sit or hit?"<<endl; 
+            cin>>hitsit;
+            if (hitsit == "?") {
+                b.print_hand();
+            } else if(hitsit == "sit") {
+                if(b.compare()) {
+                    cout << "You lose!" << endl;
+                    //lose chips
+                    gameStatus = 1;
             } else {
                 cout << "You win!" << endl;
-                //gain chips            
+                //gain chips
+                gameStatus = 1;            
             }
         } 
         else if (hitsit=="hit") {
             b.recieve_card();
             if (b.went_bust()) {
                 cout << "BUST, house wins!" << endl;
-                //Lost all bet chips
-                lost = 1;
+                //lose all bet chips
+                gameStatus = 1;
             }
         } else {
             cout << "Input must be 'hit', 'sit', or '?'." << endl;
         }
     }
+    }
+
+    
+    
 
 };
