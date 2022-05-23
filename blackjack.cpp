b0faac338c26f168e48f84e1536f333104210e2a
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 11:33:36 +0930

    made with makefile

--- blackjack.cpp
+++ blackjack.cpp
@@ -8,9 +8,11 @@
 
 
 
+
 blackjack::blackjack() {
     cout << "You have chosen to play blackjack. Here are the rules:" << endl << "*rules*" << endl;                    // insert game rules
     cout<< "Enter the '?' key when asked to sit or hit to view your hand"<<endl;
+    Deck deck;
     int cardIndex = rand() % 10;    //make rand -> function to return random number between x and y
     int aboveNumberIndex = 0;
     if (cardIndex==9) {
@@ -20,10 +22,10 @@ blackjack::blackjack() {
     house = (rand() % 10 + 1)+(rand() % 10 + 1);
     player = cardIndex+1;           //cardIndex+1 since cardIndex is used in the cards array
     round = 1;
-    Deck.hand[0][0] = Deck.cards[cardIndex + aboveNumberIndex]; 
-    Deck.hand[0][1] = Deck.suit[suitIndex];
+    deck.hand[0][0] = deck.cards[cardIndex + aboveNumberIndex]; 
+    deck.hand[0][1] = deck.suit[suitIndex];
     
-    cout << "You have: " << Deck.cards[cardIndex + aboveNumberIndex] << Deck.suit[suitIndex] << endl; //removed " of " since we will add symbolic expression
+    cout << "You have: " << deck.cards[cardIndex + aboveNumberIndex] << deck.suit[suitIndex] << endl; //removed " of " since we will add symbolic expression
  
 }
 
@@ -45,9 +47,9 @@ void blackjack::recieve_card() {    //would be good if this is a function to be 
         aboveNumberIndex = rand() % 3;
     }
     int suitIndex = rand() % 4;
-    cout << "Recieved: " << Deck.cards[cardIndex + aboveNumberIndex] << Deck.suit[suitIndex]<<endl;
-    Deck.hand[round][0] = Deck.cards[cardIndex + aboveNumberIndex];
-    Deck.hand[round][1] = Deck.suit[suitIndex];
+    cout << "Recieved: " << deck.cards[cardIndex + aboveNumberIndex] << deck.suit[suitIndex]<<endl;
+    deck.hand[round][0] = deck.cards[cardIndex + aboveNumberIndex];
+    deck.hand[round][1] = deck.suit[suitIndex];
     player = player + cardIndex + 1;
 
     round++;       
@@ -55,7 +57,7 @@ void blackjack::recieve_card() {    //would be good if this is a function to be 
 
 void blackjack::print_hand() {
     for (int i = 0; i < round; i++) {
-        cout << Deck.hand[i][0] << " of " << Deck.hand[i][1] << endl;
+        cout << deck.hand[i][0] << " of " << deck.hand[i][1] << endl;
     }
 }
 
