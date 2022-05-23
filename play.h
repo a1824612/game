da879a48439fa0e3e3ce1d7c7d1d723124b0b2e9
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 12:46:15 +0930

    made with makefile

--- play.h
+++ play.h
@@ -3,9 +3,27 @@
 
 #include <string> 
 #include "blackjack.h"
-#include "deck.h"
+#include "chip.h"
+
+class Play: public chip{
 
 
+
+
+    public: 
+        void plus_chips(){cout<<"poo";};    //these functions will have differtent implementations in each game
+        void minus_chips(){cout<<"poo";};
+
+
+
+
+
+
+
+
+
+
+};
 
 
 
