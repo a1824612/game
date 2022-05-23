c085c5733252bfe396383a6ea7c696f68e0cc59f
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 11:25:47 +0930

    made with makefile

--- deck.h
+++ deck.h
@@ -14,7 +14,7 @@ class Deck{
         string hand[10][2];
         string cards[13] = {"ace","2","3","4","5","6","7","8","9","10","jack","queen","king"};
         string suit[4] = {"diamonds","clubs","spaids","hearts"};    //replace with symbols
-        deck();
+        Deck();
 
 
 };
