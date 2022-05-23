db8fb675dec58a6570b089657ba05e4b04876c3f
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 15:01:19 +0930

    made with makefile

--- betEdit.cpp
+++ betEdit.cpp
@@ -2,9 +2,9 @@
 #include "betEdit.h"
 
 void betEdit::plus_chips(int bet) {
-    chips = chips + bet;
+    game1.chips = game1.chips + bet;
 }
 
 void betEdit::minus_chips(int bet) {
-    chips = chips - bet;
+    game1.chips = game1.chips - bet;
 }
