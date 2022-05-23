9fcf0a66d081383a8d33b0a40d1e74869f3dfc04
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 15:31:02 +0930

    made with makefile

--- make.mk
+++ make.mk
@@ -1,4 +1,4 @@
-git add main.cpp casino.cpp casino.h blackjack.cpp blackjack.h chip.cpp chip.h play.cpp play.h deck.cpp deck.h
+git add main.cpp betEdit.cpp betEdit.h casino.cpp casino.h blackjack.cpp blackjack.h chip.cpp chip.h play.cpp play.h deck.cpp deck.h
 git commit -m "made with makefile"
-g++ main.cpp casino.cpp casino.h blackjack.cpp blackjack.h chip.cpp chip.h play.cpp play.h deck.cpp deck.h -o 1
+g++ main.cpp betEdit.cpp betEdit.h casino.cpp casino.h blackjack.cpp blackjack.h chip.cpp chip.h play.cpp play.h deck.cpp deck.h -o 1
 ./1
