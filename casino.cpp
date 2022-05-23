59deefb98641be25e98b49e69298c2aaa482d7e9
a1824612 <a1824612@student.adelaide.edu.au>
2022-05-23 12:05:46 +0930

    made with makefile

--- casino.cpp
+++ casino.cpp
@@ -5,7 +5,7 @@ using namespace std;
 casino::casino(){
    chips = 1000;
     cout<<"WELCOME TO BALCKJACK!"<<endl; 
-    cout<<"We have given you 1000 chips to play with.";
+    cout<<"We have given you 1000 chips to play with." << endl;
 }
 
  int casino::get_chips(){
